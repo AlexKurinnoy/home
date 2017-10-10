9d51d2dda37d776e8e67c025d0b263b8b16b031c
AlexKurinnoy <a.n.kurinnoy@gmail.com>
2017-10-10 14:32:40 +0300

    Initial commit

---
+++ MyFirstApp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MyFirstApp{
+	public static void main(String[] args){
+
+		while(true){
+			System.out.println("Enter number");
+			Scanner input = new Scanner(System.in);
+			int x=input.nextInt()%2;
+			if (x == 0) {
+				System.out.println("Even number");
+				return;
+			}
+			else{
+				System.out.println("odd number");
+				return;
+			}
+		}
+
+	}
+}
